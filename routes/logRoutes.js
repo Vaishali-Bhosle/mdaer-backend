2a2c38644eb249067e25181ec58571c725081268
Samhita P <samhita2004sam@gmail.com>
2025-03-06 15:33:10 +0530

    I removed all previous files and made this the best ever repository

--- routes/logRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const logController = require('../controllers/logController');
-const authMiddleware = require('../middleware/authMiddleware');
-const roleMiddleware = require('../middleware/roleMiddleware');
-
-router.get('/', authMiddleware, roleMiddleware(['institution_admin', 'department_admin']), logController.getAllLogs);
-
-module.exports = router;
