bb02a855e7ac732343c4a36fbf23acc8e358cd91
Samhita P <samhita2004sam@gmail.com>
2025-03-11 17:40:53 +0530

    deleted unwanted files

--- users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RegisterUserView, UpdateUserView
-
-urlpatterns = [
-    path('register/', RegisterUserView.as_view(), name='register'),
-    path('update/<str:username>/', UpdateUserView.as_view(), name='update-user'),
-]
