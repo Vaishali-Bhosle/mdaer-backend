c553de6855a8a5a23fbd287229f03dc1ca1a2aff
Thanusha2284 <thanusha2284@gmail.com>
2025-03-12 15:15:18 +0530

    adding my project and app

--- reg_log/reg_log_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RegLogAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reg_log_app'
--- reg_log/reg_log_app/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsUserOrReadOnly(BasePermission):
-    """
-    Custom permission to allow only users to update their own profile.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        return obj.username == request.user.username
